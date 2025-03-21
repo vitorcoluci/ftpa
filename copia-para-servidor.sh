33e952bc18b5940a062915a797ec9b88e9799264
Vitor Coluci <coluci@unicamp.br>
2025-03-21 10:57:05 -0300

    a

--- copia-para-servidor.sh
+++ copia-para-servidor.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+cd _site/
 cp index.html robots.txt search.json sitemap.xml styles.css ~/Downloads/ftpa-planejamento/
 cp -r atividades custos dados inscricao justificativa localizacao organizacao publico-alvo site_libs sobre ~/Downloads/ftpa-planejamento/
-cd ~/Downloads/ftpa-planejamento
-git commit -am "subindo website"
-git push origin main
+# executar os comandos abaixo na pasta do ftpa-planejameto
+#git commit -am "subindo website"
+#git push origin main
+
